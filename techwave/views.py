c0760f2d2420eabeb734e5f25de7895bf8cf44d7
julo57 <julian.mikuskiewicz@gmail.com>
2024-02-15 22:59:13 +0100

    Wrzucenie zdjecia

--- techwave/views.py
+++ techwave/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Product
+import random
 
 def home(request):
-    return render(request, 'Techwave/home.html', {'title': 'Home'})
+    context = {'title': 'Home'}  # Include title in the context
+    try:
+        products = list(Product.objects.all())
+        random.shuffle(products)  # Shuffle products
+        displayed_products = products[:5]  # Select first 5
+        context['displayed_products'] = displayed_products
+        context['error'] = None
+    except Exception as e:
+        context['error'] = str(e)
+        context['displayed_products'] = []
+
+    # Make sure to adjust 'Techwave/home.html' to the correct path if necessary
+    return render(request, 'Techwave/home.html', context)
